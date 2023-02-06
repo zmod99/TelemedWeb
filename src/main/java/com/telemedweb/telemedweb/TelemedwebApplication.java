ff58843567e74759359aee10e2a830b99e62f83f
mmesaric <maria.mesaric1234@gmail.com>
2023-02-06 14:25:46 +0100

    Initial commit

---
+++ src/main/java/com/telemedweb/telemedweb/TelemedwebApplication.java
@@ -0,0 +1,13 @@
+package com.telemedweb.telemedweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelemedwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelemedwebApplication.class, args);
+	}
+
+}
