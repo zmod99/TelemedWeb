c4cc39557d5fa6e16eb4b8313c7f3d3e4cecdb0a
mmesaric <maria.mesaric1234@gmail.com>
2023-02-06 15:43:00 +0100

    Novi commit fresh

---
+++ src/test/java/com/telemedweb/telemedweb/TelemedwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telemedweb.telemedweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelemedwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
